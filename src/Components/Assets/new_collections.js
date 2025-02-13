f83932c134d9f40507b36d6a85332a2765f16202
Divya-2130 <divyasriyadava@gmail.com>
2025-02-13 21:59:42 +0530

    react.js

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -8,56 +8,56 @@ import k3_img from './k3.png';
 import k8_img from './k8.png';
 const new_collections = [
     {
-        id:1,
+        id:22,
         name:"DELU DRESSES Baby Girls Casual Jeans Top Price in",
         image: k4_img,
         new_price: 700.99,
         old_price: 1200.33
     },
     {
-        id:2,
+        id:8,
         name:"Trendy Kurti with Jeans Ideas to pair for Ladies &",
         image: w8_img,
         new_price: 1669.99,
         old_price: 2700.33
     },
     {
-        id:3,
+        id:18,
         name:"This item: Lymio Men's Printed Regular Fit T-Shirt",
         image: m10_img,
         new_price: 569.99,
         old_price: 900.93
     },
     {
-        id:4,
+        id:7,
         name:"grrreny colour eith butterfly design|with bottom |gold print",
         image: w7_img,
         new_price: 1569.99,
         old_price: 2400.33
     },
     {
-        id:5,
+        id:23,
         name:"blue frok for the bule lover",
         image: k5_img,
         new_price: 969.99,
         old_price: 1600.33
     },
     {
-        id:6,
+        id:17,
         name:"Men Premium ReadyMade Soft Silk Panchakacham & Towel Set Pranav",
         image: m9_img,
         new_price: 869.99,
         old_price: 1300.33
     },
     {
-        id:7,
+        id:21,
         name:"royal blue frock for the kids|with round neck|modren design",
         image: k3_img,
         new_price: 1669.99,
         old_price: 2700.33
     },
     {
-        id:8,
+        id:28,
         name:"kids with trendy fasion|stylish suit|with round neck",
         image: k8_img,
         new_price: 569.99,
